44bcef9c7c8d06236e82b8f42972b4f690bd526d
jdev <jajangnurjmn04@gmail.com>
2026-08-07 04:10:19 +0700

    Update service worker cache version to v2.0

--- sw.js
+++ sw.js
@@ -1,32 +1,29 @@
-const CACHE_NAME = 'inkflow-v1.0';
+const CACHE_NAME = 'inkflow-v2.0'; // GANTI versi!
 const ASSETS_TO_CACHE = [
     './',
     './index.html',
     './style.css',
     './app.js',
+    './sw.js',
     './manifest.json',
     './icon-192.png',
     './icon-512.png'
 ];
 
-// Tahap Install: Memasukkan file ke dalam Cache
 self.addEventListener('install', (event) => {
     event.waitUntil(
         caches.open(CACHE_NAME).then((cache) => {
-            console.log('Service Worker: Caching Files');
             return cache.addAll(ASSETS_TO_CACHE);
         })
     );
 });
 
-// Tahap Activate: Membersihkan Cache versi lama (jika ada update)
 self.addEventListener('activate', (event) => {
     event.waitUntil(
         caches.keys().then((cacheNames) => {
             return Promise.all(
                 cacheNames.map((cache) => {
                     if (cache !== CACHE_NAME) {
-                        console.log('Service Worker: Clearing Old Cache');
                         return caches.delete(cache);
                     }
                 })
@@ -35,7 +32,6 @@ self.addEventListener('activate', (event) => {
     );
 });
 
-// Tahap Fetch: Mengambil dari Cache dulu saat tidak ada internet
 self.addEventListener('fetch', (event) => {
     event.respondWith(
         caches.match(event.request).then((response) => {
